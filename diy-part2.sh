07fae3cdf9b23e3d829f03f8772fae502b069637
craytona170 <121808362+craytona170@users.noreply.github.com>
2023-01-06 02:48:28 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,3 +17,4 @@ sed -i "s/OpenWrt /HI build $(TZ=UTC-8 date "+%Y.%m.%d") @ R4AGV2 /g" package/le
 sed -i '/uci commit system/i\uci set system.@system[0].hostname='XiaoMiRouter'' package/lean/default-settings/files/zzz-default-settings
 sed -i 's/encryption=none/encryption=sae-mixed/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 sed -i '/set wireless.default_radio${devidx}.encryption=sae-mixed/a\set wireless.default_radio${devidx}.key=19991014' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i "/exit 0/i\sed -i '/coremark/d' /etc/crontabs/root" "${FIN_PATH}"
