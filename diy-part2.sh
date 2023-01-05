c3e8ec0751ff07f0ea66a77cd61b8c2d2b48d23f
craytona170 <121808362+craytona170@users.noreply.github.com>
2023-01-05 13:42:51 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,3 +14,4 @@
 sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
 sed -i 's/ssid=OpenWrt/ssid=WiFi_R4AGv2/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 sed -i "s/OpenWrt /HI build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
+sed -i '/uci commit system/i\uci set system.@system[0].hostname='XiaoMi Router'' package/lean/default-settings/files/zzz-default-settings
