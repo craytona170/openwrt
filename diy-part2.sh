e706177d5983c719c6b4b564dce7865ce930861a
craytona170 <121808362+craytona170@users.noreply.github.com>
2023-08-06 18:06:50 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,6 +11,6 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.31.2/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.4/g' package/base-files/files/bin/config_generate
 sed -i 's/ssid=OpenWrt/ssid=WiFi_R4AGv2/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 sed -i "s/OpenWrt /HI build $(TZ=UTC-8 date "+%Y.%m.%d") @ X86 /g" package/lean/default-settings/files/zzz-default-settings
